fe1b9767266f2b1eb8e324f4ba1712ee1c512d36
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2023-08-31 16:55:16 -0400

    none

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//let lista= ["peixe", "brocolis", "mortadela", 1, 3, "pao"]
+const aleatorio= function(){
+  
+}
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
+});
+
+app.get('/produtos', (req, res) => {
+  res.render('produtos', { message: 'Olá, Mundo!', lista: ["peixe", "brocolis", "mortadela", 1, 3, "pao"]});
 });
 
 app.listen(port, () => {
